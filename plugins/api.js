123ad80081d6766b0b5c4c277dbd6d3f6a069b7a
TURAROVICH <turaaa555@gmail.com>
2022-04-03 01:30:10 +0600

    req api url changed

--- plugins/api.js
+++ plugins/api.js
@@ -1,5 +1,5 @@
 export default function ({ $axios }, inject) {
     const api = $axios.create();
-    api.setBaseURL('http://192.168.0.100:8000')
+    api.setBaseURL('http://193.187.173.78:8000')
     inject('api', api)
 }
