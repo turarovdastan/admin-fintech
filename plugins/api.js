207adacf55f1ae047eea075855f813174d2f11ed
TURAROVICH <turaaa555@gmail.com>
2022-04-05 21:00:39 +0600

    admin panel done

--- plugins/api.js
+++ plugins/api.js
@@ -1,5 +1,9 @@
-export default function ({ $axios }, inject) {
+export default function ({ $axios, app, redirect }, inject) {
     const api = $axios.create();
     api.setBaseURL(process.env.NODE_ENV !== 'production' ? 'http://localhost:8000' : 'http://193.187.173.78:8000')
+    api.interceptors.response.use((response) => response, (error) => {
+        app.$cookies.remove('token')
+        redirect('/login')
+    });
     inject('api', api)
 }
